ae2a3cad544a81acaa34f1e3197055831f0b5518
Duntron1000 <lpbruhin@gmail.com>
2024-01-08 07:18:15 -0500

    first commit

---
+++ lua/core/init.lua
@@ -0,0 +1,4 @@
+print("hello from the core")
+require("core.remap")
+require("core.lazy")
+require("core.set")
---
+++ lua/core/plugins/whichkey.lua
@@ -0,0 +1,25 @@
+return {
+
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        local wk = require("which-key")
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+
+        wk.register({
+          f = {
+            name = "Find",
+            f = "Find", 
+            g = "Grep",
+            h = "Help Docs",
+          },
+        }, { prefix = "<leader>" } )
+
+    end,
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    }
+}
