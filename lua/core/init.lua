a529beb81f66fbbf1f7c040dad3a47f9b43d80ab
Duntron1000 <lpbruhin@gmail.com>
2024-07-11 18:07:06 -0400

    Lsp stuff

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,4 +1,3 @@
-print("hello from the core")
 require("core.remap")
 require("core.lazy")
 require("core.set")
--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+
+-- compile the current buffer with gcc
+vim.keymap.set("n", "<leader>cc", string.format(":!gcc %s -o %s<ENTER>", vim.api.nvim_buf_get_name(0), string.sub(vim.api.nvim_buf_get_name(0), 0, string.len(vim.api.nvim_buf_get_name(0)) - 2)))
+vim.keymap.set("n", "<leader>cr", string.format(":!%s<ENTER>", string.sub(vim.api.nvim_buf_get_name(0), 0, string.len(vim.api.nvim_buf_get_name(0)) - 2)))
+vim.keymap.set("n", "<leader>cm", ":!make<ENTER>")
